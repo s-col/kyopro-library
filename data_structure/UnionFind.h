287636dd84556a5f6a75bf4a83ddab4f87e06519
scol <ss.scol.net@gmail.com>
2020-03-02 01:48:52 +0900

    UnionFindで素集合のsizeをとれるようにした

--- data_structure/UnionFind.h
+++ data_structure/UnionFind.h
@@ -4,28 +4,31 @@ template <typename _Ty>
 class UnionFind {
 private:
     vector<_Ty> par;
-    vector<_Ty> rank;
+    vector<_Ty> sz;
 
 public:
-    UnionFind(_Ty N) : par(N), rank(N) {
+    UnionFind(_Ty N) : par(N), sz(N, 1) {
         for (_Ty i = 0; i < N; ++i) par[i] = i;
     }
 
-    _Ty root(_Ty x) { return par[x] == x ? x : par[x] = root(par[x]); }
+    _Ty root(_Ty x) {
+        return par[x] == x ? x : par[x] = root(par[x]);
+    }
 
-    bool same(_Ty x, _Ty y) { return root(x) == root(y); }
+    bool same(_Ty x, _Ty y) {
+        return root(x) == root(y);
+    }
 
     void unite(_Ty x, _Ty y) {
         x = root(x);
         y = root(y);
         if (x == y) return;
+        if (sz[x] < sz[y]) swap(x, y);
+        par[y] = x;
+        sz[x] += sz[y];
+    }
 
-        if (rank[x] < rank[y]) {
-            par[x] = y;
-        }
-        else {
-            par[y] = x;
-            if (rank[x] == rank[y]) ++rank[x];
-        }
+    _Ty size(_Ty x) {
+        return sz[root(x)];
     }
 };

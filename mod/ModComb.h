1426774771009c3c73abee290f8e3590bcce48f4
scol <ss.scol.net@gmail.com>
2020-06-28 03:09:53 +0900

    ModCombの内部の変数をModIntに統一した また、permutationを求めるメンバ関数を追加した

--- mod/ModComb.h
+++ mod/ModComb.h
@@ -7,10 +7,10 @@
 template <std::int_fast64_t Modulus>
 class ModComb {
     using i64 = int_fast64_t;
-    using Vl = std::vector<i64>;
+    using Vm = std::vector<ModInt<Modulus>>;
 
 private:
-    Vl fac, inv, finv;
+    Vm fac, inv, finv;
 
 public:
     explicit ModComb(i64 n) noexcept : fac(n + 1), inv(n + 1), finv(n + 1) {
@@ -18,23 +18,30 @@ public:
         finv[0] = finv[1] = 1;
         inv[1] = 1;
         for (i64 i = 2; i < n + 1; ++i) {
-            fac[i] = fac[i - 1] * i % Modulus;
-            inv[i] = Modulus - inv[Modulus % i] * (Modulus / i) % Modulus;
-            finv[i] = finv[i - 1] * inv[i] % Modulus;
+            fac[i] = fac[i - 1] * i;
+            inv[i] = Modulus - inv[Modulus % i] * (Modulus / i);
+            finv[i] = finv[i - 1] * inv[i];
         }
     }
 
     ModInt<Modulus> comb(i64 n, i64 k) const noexcept {
         ModInt<Modulus> res;
-        if (n < k)
-            res = 0;
-        else if (n < 0 || k < 0)
+        if (n < k || n < 0 || k < 0)
             res = 0;
         else
-            res = fac[n] * (finv[n - k] * finv[k] % Modulus) % Modulus;
+            res = fac[n] * finv[n - k] * finv[k];
         return res;
     }
 
-    ModInt<Modulus> get_fac(i64 n) const noexcept { return ModInt<Modulus>(fac[n]); }
-    ModInt<Modulus> get_finv(i64 n) const noexcept { return ModInt<Modulus>(finv[n]); }
+    ModInt<Modulus> perm(i64 n, i64 k) const noexcept {
+        ModInt<Modulus> res;
+        if (n < k || n < 0 || k < 0)
+            res = 0;
+        else
+            res = fac[n] * finv[n - k];
+        return res;
+    }
+
+    ModInt<Modulus> get_fac(i64 n) const noexcept { return fac[n]; }
+    ModInt<Modulus> get_finv(i64 n) const noexcept { return finv[n]; }
 };

fc8f493d52d2f0e0cc73a336a93a6a063052b380
scol <ss.scol.net@gmail.com>
2020-07-01 03:02:03 +0900

    xorshift.hを追加

---
+++ etc/xorshift.h
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+struct XorShift {
+    uint32_t x = 123456789;
+    uint32_t y = 362436069;
+    uint32_t z = 521288629;
+    uint32_t w = 88675123;
+    double urand(void) noexcept {
+        uint32_t t;
+        t = x ^ (x << 11);
+        x = y;
+        y = z;
+        z = w;
+        w ^= t ^ (t >> 8) ^ (w >> 19);
+        return w / 4294967296.0;
+    }
+    int randint(int a, int b) noexcept {
+        return static_cast<int>(urand() * (b - a + 1)) + a;
+    }
+};

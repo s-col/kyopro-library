3f5412a6f141b98d92ea6f5a49beb0761e2cfb16
s-col <ss.scol.net@gmail.com>
2020-12-28 23:18:59 +0900

    Update LasySegmentTree

--- data_structure/LazySegmentTree.h
+++ data_structure/LazySegmentTree.h
@@ -19,6 +19,7 @@
 template <class T, class E, class F, class G, class H, class P>
 class LazySegmentTree {
 private:
+    const int n;
     const F f;
     const G g;
     const H h;
@@ -32,17 +33,34 @@ private:
 public:
     explicit LazySegmentTree(
         int n, F f, G g, H h, P p, T tid, E eid) noexcept
-        : f(f), g(g), h(h), p(p), tid(tid), eid(eid) {
+        : n(n), f(f), g(g), h(h), p(p), tid(tid), eid(eid) {
         sz = 1;
         while (sz < n) sz <<= 1;
         vec.assign(sz * 2, tid);
         lazy.assign(sz * 2, eid);
+    }
+    explicit LazySegmentTree(
+        const std::vector<T>& vec, F f, G g, H h, P p, T tid, E eid) noexcept
+        : n(vec.size()), f(f), g(g), h(h), p(p), tid(tid), eid(eid) {
+        sz = 1;
+        while (sz < n) sz <<= 1;
+        this->vec.assign(sz * 2, tid);
+        lazy.assign(sz * 2, eid);
+        set_array(vec);
     }
     T& operator[](int idx) noexcept {
         return vec[sz + idx];
     }
     void set_value(int idx, T val) noexcept {
         vec[sz + idx] = val;
+    }
+    template <class RandomIt>
+    void set_array(RandomIt _begin, RandomIt _end) noexcept {
+        std::copy(_begin, _end, vec.begin() + sz);
+    }
+    template <class Vec>
+    void set_array(const Vec& v) noexcept {
+        set_array(std::begin(v), std::end(v));
     }
     void build() noexcept {
         for (int i = sz - 1; i > 0; i--) {
@@ -57,6 +75,20 @@ public:
     }
     T query_all() noexcept {
         return query(0, sz);
+    }
+    // Return the largest x such that check(A[idx] op ... op A[x - 1]) == true
+    // complexity: O(log (n))
+    template <class C>
+    int max_right(int idx, const C& check) noexcept {
+        T acc = tid;
+        return idx < n ? _max_right(idx, check, acc, 1, 0, sz) : n;
+    }
+    // Return the smallest x such that check(A[x] op ... op A[idx - 1]) == true
+    // complexity: O(log (n))
+    template <class C>
+    int min_left(int idx, const C& check) noexcept {
+        T acc = tid;
+        return idx > 0 ? _min_left(idx, check, acc, 1, 0, sz) : 0;
     }
 
 private:
@@ -93,5 +125,45 @@ private:
             T t2 = _query(a, b, (k << 1) | 1, (l + r) >> 1, r);
             return f(t1, t2);
         }
+    }
+    template <class C>
+    int _max_right(int idx, const C& check, T& acc, int k, int l, int r) noexcept {
+        eval(k, l, r);
+        if (l + 1 == r) {
+            acc = f(acc, vec[k]);
+            return check(acc) ? n : k - sz;
+        }
+        const int mid = (l + r) >> 1;
+        if (mid <= idx) return _max_right(idx, check, acc, (k << 1) | 1, mid, r);
+        if (idx <= l) {
+            T tmp = f(acc, vec[k]);
+            if (check(tmp)) {
+                acc = tmp;
+                return n;
+            }
+        }
+        int vl = _max_right(idx, check, acc, k << 1, l, mid);
+        if (vl < n) return vl;
+        return _max_right(idx, check, acc, (k << 1) | 1, mid, r);
+    }
+    template <class C>
+    int _min_left(int idx, const C& check, T& acc, int k, int l, int r) const noexcept {
+        eval(k, l, r);
+        if (l + 1 == r) {
+            acc = f(acc, vec[k]);
+            return check(acc) ? 0 : k - sz + 1;
+        }
+        const int mid = (l + r) >> 1;
+        if (mid >= idx) return _min_left(idx, check, acc, k << 1, l, mid);
+        if (idx >= r) {
+            T tmp = f(acc, vec[k]);
+            if (check(tmp)) {
+                acc = tmp;
+                return 0;
+            }
+        }
+        int vr = _min_left(idx, check, acc, (k << 1) | 1, mid, r);
+        if (vr > 0) return vr;
+        return _min_left(idx, check, acc, k << 1, l, mid);
     }
 };

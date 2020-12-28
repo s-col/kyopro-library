cf518802f0138d4ad2f87ae45d9fff44b6447b36
s-col <ss.scol.net@gmail.com>
2020-12-28 19:54:02 +0900

    Add max_right() to segment tree

--- data_structure/SegmentTree.h
+++ data_structure/SegmentTree.h
@@ -11,6 +11,7 @@
 template <class T, class Op>
 class SegmentTree {
 private:
+    const int n;
     const Op op;
     const T id;
     std::vector<T> vec;
@@ -18,17 +19,17 @@ private:
 
 public:
     explicit SegmentTree(int n, Op op, T id) noexcept
-        : op(op), id(id) {
+        : n(n), op(op), id(id) {
         sz = 1;
         while (sz < n) sz <<= 1;
-        vec.assign(sz * 2, id);
+        vec.assign(sz << 1, id);
     }
     explicit SegmentTree(const std::vector<T>& vec, Op op, T id) noexcept
         : op(op), id(id) {
         int n = vec.size();
         sz = 1;
         while (sz < n) sz <<= 1;
-        this->vec.assign(sz * 2, id);
+        this->vec.assign(sz << 1, id);
         set_array(vec);
     }
     T& operator[](int idx) noexcept {
@@ -46,14 +47,14 @@ public:
     }
     void build() noexcept {
         for (int i = sz - 1; i > 0; i--) {
-            vec[i] = op(vec[i * 2], vec[i * 2 + 1]);
+            vec[i] = op(vec[i << 1], vec[(i << 1) | 1]);
         }
     }
     void update(int idx, T val) noexcept {
         idx += sz;
         vec[idx] = val;
         for (idx >>= 1; idx > 0; idx >>= 1) {
-            vec[idx] = op(vec[idx * 2], vec[idx * 2 + 1]);
+            vec[idx] = op(vec[idx << 1], vec[(idx << 1) | 1]);
         }
     }
     T query(int l, int r) const noexcept {
@@ -65,7 +66,41 @@ public:
         }
         return op(l_val, r_val);
     }
+    T query_all() const noexcept {
+        return query(0, sz);
+    }
+    // Return the largest x such that check(A[idx] op ... op A[x - 1]) == true
+    // complexity: O(log (n))
+    template <class F>
+    int max_right(int idx, const F& check) const noexcept {
+        T acc = id;
+        return _max_right(idx, check, acc, 1, 0, sz);
+    }
     void reset() noexcept {
         std::fill(vec.begin(), vec.end(), id);
+    }
+
+private:
+    template <class F>
+    int _max_right(int idx, const F& check, T& acc, int k, int l, int r) const noexcept {
+        if (l + 1 == r) {
+            acc = op(acc, vec[k]);
+            return check(acc) ? n : k - sz;
+        }
+        const int mid = (l + r) >> 1;
+        if (mid <= idx)
+            return _max_right(idx, check, acc, (k << 1) | 1, mid, r);
+        if (idx <= l) {
+            T tmp = op(acc, vec[k]);
+            if (check(tmp)) {
+                acc = tmp;
+                return n;
+            }
+        }
+        int vl = _max_right(idx, check, acc, k << 1, l, mid);
+        if (vl < n) {
+            return vl;
+        }
+        return _max_right(idx, check, acc, (k << 1) | 1, mid, r);
     }
 };

4998928dd1c10801d927c3bdca44e35348a699ee
s-col <ss.scol.net@gmail.com>
2021-04-17 20:09:09 +0900

    add CompressedCoordinate

---
+++ etc/CompressedCoordinate.h
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+template <typename T>
+class CompressedCoordinate {
+private:
+    std::vector<T> pos;
+
+public:
+    CompressedCoordinate() noexcept {}
+    void add(const T& p_new) noexcept {
+        pos.emplace_back(p_new);
+    }
+    void build() noexcept {
+        std::sort(pos.begin(), pos.end());
+        pos.erase(std::unique(pos.begin(), pos.end()), pos.end());
+    }
+    size_t lower_bound(const T& key) const noexcept {
+        return std::lower_bound(pos.begin(), pos.end(), key) - pos.begin();
+    }
+    size_t upper_bound(const T& key) const noexcept {
+        return std::upper_bound(pos.begin(), pos.end(), key) - pos.beegin();
+    }
+    size_t operator[](const T& key) const noexcept {
+        return lower_bound(key);
+    }
+    size_t size() const noexcept {
+        return pos.size();
+    }
+};

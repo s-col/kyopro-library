e60287e3c3fa46988c4b8272b516c1f641c7a0a8
s-col <ss.scol.net@gmail.com>
2021-08-13 21:10:04 +0900

    add annealing.cpp

---
+++ heuristic/annealing.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+void solve_annealing() {
+    const auto t_start = std::chrono::system_clock::now();
+    const auto T_LIMIT = std::chrono::milliseconds(2000);
+
+    constexpr double ALPHA = 0.9999995;
+    double temp = 1000;
+
+    constexpr int n_time = 1000;
+    int i_time = 0;
+
+    int64_t count = 0, update_count = 0;
+    while (true) {
+        /*
+             *- code -* 
+        */
+        ++count;
+        ++i_time;
+        if (i_time == n_time) {
+            if (std::chrono::system_clock::now() - t_start >= T_LIMIT) {
+                break;
+            }
+        }
+    }
+    std::cerr << "count:\t" << count << "\n";
+    std::cerr << "update:\t" << update_count << "\n";
+    std::cerr << "temp:\t" << temp << "\n";
+}

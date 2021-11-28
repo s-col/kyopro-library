285d8b6100fb3ec1e00b8655ec2d77e6b14bf7a6
s-col <ss.scol.net@gmail.com>
2021-11-28 22:58:02 +0900

    XorShift を改善

--- etc/xorshift.h
+++ etc/xorshift.h
@@ -5,17 +5,20 @@ struct XorShift {
     uint32_t y = 362436069;
     uint32_t z = 521288629;
     uint32_t w = 88675123;
-    double urand(void) noexcept {
+    uint32_t raw_rand(void) noexcept {
         uint32_t t;
         t = x ^ (x << 11);
         x = y;
         y = z;
         z = w;
         w ^= t ^ (t >> 8) ^ (w >> 19);
-        return w / 4294967296.0;
+        return w;
     }
-    // note: [a, b]
+    double urand(void) noexcept {
+        return raw_rand() / 4294967296.0;
+    }
+    // note: [a, b)
     int randint(int a, int b) noexcept {
-        return static_cast<int>(urand() * (b - a + 1)) + a;
+        return raw_rand() % (b - a) + a;
     }
 };

0b473bf299f85e99aed7b2179c874654a320ec85
s-col <ss.scol.net@gmail.com>
2021-12-19 19:12:10 +0900

    重み付きグラフを修正

--- graph/template.h
+++ graph/template.h
@@ -2,11 +2,12 @@
 
 template <typename T>
 struct WeightedEdge {
-    int to;
+    int from, to;
     T cost;
-    constexpr Edge(int to, T cost = -1) noexcept : to(to), cost(cost) {}
+    constexpr WeightedEdge(int to, T cost) noexcept : from(-1), to(to), cost(cost) {}
+    constexpr WeightedEdge(int from, int to, T cost) noexcept : from(from), to(to), cost(cost) {}
 };
 template <typename T>
-using WeightedGraph = std::vector<std::vector<WeightedEdge>>;
+using WeightedGraph = std::vector<std::vector<WeightedEdge<T>>>;
 
 using Graph = std::vector<std::vector<int>>;

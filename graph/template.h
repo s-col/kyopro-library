447dc006acb37bc0314ca44ae43e5a2fcd75e179
s-col <ss.scol.net@gmail.com>
2023-06-24 23:37:49 +0900

    重み付き辺の比較演算子を定義

--- graph/template.h
+++ graph/template.h
@@ -7,7 +7,17 @@ struct WeightedEdge {
     constexpr WeightedEdge(int to, T cost) noexcept : from(-1), to(to), cost(cost) {}
     constexpr WeightedEdge(int from, int to, T cost) noexcept : from(from), to(to), cost(cost) {}
 };
+struct WeightedEdgeLess {
+    template <typename T>
+    constexpr bool operator()(const WeightedEdge<T>& e1, const WeightedEdge<T>& e2) const noexcept {
+        return e1.cost < e2.cost;
+    }
+};
+struct WeightEdgeGreater {
+    template <typename T>
+    constexpr bool operator()(const WeightedEdge<T>& e1, const WeightedEdge<T>& e2) const noexcept {
+        return e1.cost > e2.cost;
+    }
+};
 template <typename T>
 using WeightedGraph = std::vector<std::vector<WeightedEdge<T>>>;
-
-using Graph = std::vector<std::vector<int>>;
